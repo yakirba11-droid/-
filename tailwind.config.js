65b6b74296b3eeafed2d7ee991e3a25b20cc4abe
yakirba11-droid <yakirba11@gmail.com>
2025-08-22 11:00:25 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          black: "#0A0A0A",
+          cream: "#E7DFCF"
+        }
+      }
+    },
   },
   plugins: [],
 }
